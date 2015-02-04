c40bb1a91d7ce9ef384fa5ad9a0943f24d12b785
cj <cjlazell@gmail.com>
2015-02-03 19:11:16 -0600

    ability to check for columns

--- lib/ability_list.rb
+++ lib/ability_list.rb
@@ -10,30 +10,33 @@ class AbilityList
   # ---
 
   # Declares that the owner can perform `verb` on `class`.
-  def can(verb, klass=nil, &block)
-    rules << [true, verb, get_class(klass), block]
+  def can(verb, klass=nil, columns=[], &block)
+    columns = [columns] unless columns.is_a? Array
+    rules << [true, verb, get_class(klass), columns, block]
   end
 
   # Inverse of `can`.
-  def cannot(verb, klass=nil, &block)
-    rules << [false, verb, get_class(klass), block]
+  def cannot(verb, klass=nil, columns=[], &block)
+    columns = [columns] unless columns.is_a? Array
+    rules << [false, verb, get_class(klass), columns, block]
   end
 
   # ---
 
   # Checks if the owner can perform `verb` on the given `object` (or class).
-  def can?(verb, object=nil)
+  def can?(verb, object=nil, columns=[])
+    columns = [columns] unless columns.is_a? Array
     rules = rules_for(verb, get_class(object))
-    rules.inject(false) do |bool, (sign, _, _, proc)|
+    rules.inject(false) do |bool, (sign, _, _, cols, proc)|
       sign ?
-        (bool || !proc ||  proc.call(object)) :  # can
-        (bool &&  proc && !proc.call(object))    # cannot
+        ((bool || !proc ||  proc.call(object)) && ((columns & cols) == columns)) :  # can
+        (bool &&  proc && !proc.call(object) && (columns.empty? || (columns & cols) != columns))    # cannot
     end
   end
 
   # Inverse of `can?`.
-  def cannot?(verb, object=nil)
-    ! can?(verb, object)
+  def cannot?(verb, object=nil, columns)
+    ! can?(verb, object, columns)
   end
 
   # ---
@@ -53,7 +56,7 @@ class AbilityList
 
   # Returns a subset of `rules` that match the given `verb` and `class`.
   def rules_for(verb, klass)
-    rules.select do |(sign, _verb, _klass, block)|
+    rules.select do |(sign, _verb, _klass, cols, block)|
       (_verb  == :manage || _verb  == verb) &&
       (_klass == :all    || _klass == klass)
     end
